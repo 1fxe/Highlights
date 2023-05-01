d78d105433b969fa9646581c63c2c94e24e54d03
1fxe <filipwedzicha@gmail.com>
2023-05-01 01:15:29 +0100

    fix: Memory leak releated to not freeing jvm string, and remove deleteref which is not needed

--- library.cpp
+++ library.cpp
@@ -1,6 +1,7 @@
 #include "library.h"
 
 #define JSTR_TO_CSTR(env, jstr) (env)->GetStringUTFChars((jstr), NULL)
+#define UTF_16_to_8(str) std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}.to_bytes(std::u16string((char16_t *) str))
 
 namespace Highlights {
     GfeSdkWrapper g_highlights;
@@ -52,34 +53,45 @@ namespace Highlights {
                 auto localeCodeJStr = (jstring) env->CallObjectMethod(localizedPairObj, localeCodeMethod);
                 auto localizedStringJStr = (jstring) env->CallObjectMethod(localizedPairObj, localizedStringMethod);
 
+                auto localeCodeStr = JSTR_TO_CSTR(env, localeCodeJStr);
+                auto localeCodeCopy = (char *) malloc(strlen(localeCodeStr) + 1);
+                strcpy_s(localeCodeCopy, strlen(localeCodeStr) + 1, localeCodeStr);
+
+                auto localizedStringStr = JSTR_TO_CSTR(env, localizedStringJStr);
+                auto localizedStringStrCopy = (char *) malloc(strlen(localizedStringStr) + 1);
+                strcpy_s(localeCodeCopy, strlen(localizedStringStr) + 1, localizedStringStr);
+
                 nameTable[j] = {
-                        JSTR_TO_CSTR(env, localeCodeJStr),
-                        JSTR_TO_CSTR(env, localizedStringJStr)
+                        localeCodeCopy,
+                        localizedStringStrCopy
                 };
 
+                (env)->ReleaseStringUTFChars(localeCodeJStr, localeCodeStr);
+                (env)->ReleaseStringUTFChars(localizedStringJStr, localizedStringStr);
             }
 
+            auto id = JSTR_TO_CSTR(env, idJStr);
+            auto idCopy = (char *) malloc(strlen(id) + 1);
+            strcpy_s(idCopy, strlen(id) + 1, id);
+
             highlight[i] = {
-                JSTR_TO_CSTR(env, idJStr),
-                userInterestBool,
-                static_cast<GfeSDK::NVGSDK_HighlightType>(
-                    env->CallIntMethod(highlightTagObj, highlightTypeValueMethod)),
-                static_cast<GfeSDK::NVGSDK_HighlightSignificance>(
-                    env->CallIntMethod(significanceObj, significanceValueMethod)),
-                nameTable,
-                static_cast<size_t>(nameTableSize)
+                    idCopy,
+                    userInterestBool,
+                    static_cast<GfeSDK::NVGSDK_HighlightType>(
+                            env->CallIntMethod(highlightTagObj, highlightTypeValueMethod)),
+                    static_cast<GfeSDK::NVGSDK_HighlightSignificance>(
+                            env->CallIntMethod(significanceObj, significanceValueMethod)),
+                    nameTable,
+                    static_cast<size_t>(nameTableSize)
             };
+
+            (env)->ReleaseStringUTFChars(idJStr, id);
         }
 
         Highlights::g_highlights.Init(gName, highlight, listSize);
 
 
         (env)->ReleaseStringUTFChars(gameName, gName);
-        (env)->DeleteLocalRef(listClass);
-        (env)->DeleteLocalRef(highlightClass);
-        (env)->DeleteLocalRef(highlightTypeEnum);
-        (env)->DeleteLocalRef(significanceEnum);
-        (env)->DeleteLocalRef(localizedPairClass);
     }
 
     void Java_dev_fxe_highlights_Highlights_shutdown(JNIEnv *, jobject) {
@@ -91,35 +103,35 @@ namespace Highlights {
     }
 
     void Java_dev_fxe_highlights_Highlights_openGroup(JNIEnv *env, jobject, jstring groupId) {
-        auto groupStr = JSTR_TO_CSTR(env, groupId);
-        Highlights::g_highlights.OnOpenGroup(groupStr);
-        (env)->ReleaseStringUTFChars(groupId, groupStr);
+        auto groupStr = (env)->GetStringCritical(groupId, nullptr);
+        Highlights::g_highlights.OnOpenGroup(UTF_16_to_8(groupStr));
+        (env)->ReleaseStringCritical(groupId, groupStr);
     }
 
     void Java_dev_fxe_highlights_Highlights_closeGroup(JNIEnv *env, jobject, jstring groupId, jboolean destroy) {
-        auto groupStr = JSTR_TO_CSTR(env, groupId);
-        Highlights::g_highlights.OnCloseGroup(groupStr, destroy);
-        (env)->ReleaseStringUTFChars(groupId, groupStr);
+        auto groupStr = (env)->GetStringCritical(groupId, nullptr);
+        Highlights::g_highlights.OnCloseGroup(UTF_16_to_8(groupStr), destroy);
+        (env)->ReleaseStringCritical(groupId, groupStr);
     }
 
     void Java_dev_fxe_highlights_Highlights_saveScreenShot(JNIEnv *env, jobject, jstring highlightId, jstring groupId) {
-        auto highlightStr = JSTR_TO_CSTR(env, highlightId);
-        auto groupStr = JSTR_TO_CSTR(env, groupId);
-        Highlights::g_highlights.OnSaveScreenshot(highlightStr, groupStr);
-
-        (env)->ReleaseStringUTFChars(highlightId, highlightStr);
-        (env)->ReleaseStringUTFChars(groupId, groupStr);
+        auto highlightStr = (env)->GetStringCritical(highlightId, nullptr);
+        auto groupStr = (env)->GetStringCritical(groupId, nullptr);
+        Highlights::g_highlights.OnSaveScreenshot(UTF_16_to_8(highlightStr), UTF_16_to_8(groupStr));
+        (env)->ReleaseStringCritical(highlightId, highlightStr);
+        (env)->ReleaseStringCritical(groupId, groupStr);
     }
 
     void Java_dev_fxe_highlights_Highlights_saveVideo(JNIEnv *env, jobject, jstring highlightId, jstring groupId,
                                                       jint startDelta, jint endDelta) {
-        auto highlightStr = JSTR_TO_CSTR(env, highlightId);
-        auto groupStr = JSTR_TO_CSTR(env, groupId);
-        Highlights::g_highlights.OnSaveVideo(highlightStr, groupStr, static_cast<int>(startDelta),
+        auto highlightStr = (env)->GetStringCritical(highlightId, nullptr);
+        auto groupStr = (env)->GetStringCritical(groupId, nullptr);
+        Highlights::g_highlights.OnSaveVideo(UTF_16_to_8(highlightStr), UTF_16_to_8(groupStr),
+                                             static_cast<int>(startDelta),
                                              static_cast<int>(endDelta));
 
-        (env)->ReleaseStringUTFChars(highlightId, highlightStr);
-        (env)->ReleaseStringUTFChars(groupId, groupStr);
+        (env)->ReleaseStringCritical(highlightId, highlightStr);
+        (env)->ReleaseStringCritical(groupId, groupStr);
     }
 
     // TODO Check this works
@@ -138,8 +150,6 @@ namespace Highlights {
 
         Highlights::g_highlights.OnGetNumHighlights(groupStr, sigFilterEnum, tagFilterEnum);
         (env)->ReleaseStringUTFChars(groupId, groupStr);
-        (env)->DeleteLocalRef(sigFilterClass);
-        (env)->DeleteLocalRef(tagFilterClass);
     }
 
     // TODO Check this works
@@ -159,14 +169,11 @@ namespace Highlights {
         for (int i = 0; i < numGroups; i++) {
             auto groupId = (jstring) env->GetObjectArrayElement(groupIds, i);
             groupIdsArray[i] = JSTR_TO_CSTR(env, groupId);
-            (env)->ReleaseStringUTFChars(groupId, groupIdsArray[i]);
         }
 
         Highlights::g_highlights.OnOpenSummary(groupIdsArray, numGroups, sigFilterEnum, tagFilterEnum);
 
         delete[] groupIdsArray;
-        (env)->DeleteLocalRef(sigFilterClass);
-        (env)->DeleteLocalRef(tagFilterClass);
     }
 
     void Java_dev_fxe_highlights_Highlights_requestLanguage(JNIEnv *, jobject) {
